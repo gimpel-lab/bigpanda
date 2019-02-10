ba4025eda78a4912add1f9025526f8b806137a88
root <root@bigpanda.gimpel.co.il>
2019-02-10 13:08:52 +0000

    next step

--- deployscript.py
+++ deployscript.py
@@ -1,58 +1,71 @@
 #!/usr/bin/python
 
-import sys
 import os
-import git
+import sys
 import logging
+import git
+import tarfile
+import urllib.request
 # import requests
 # import subprocess
-# import tarfile
 # import time
 
-current_directory = (os.path.dirname(os.path.realpath(__file__)))
-log_filename = current_directory + '/bigpandaio-deploy.log'
-
-print('Log file: ', log_filename)
-print('Current directory: ', current_directory)
-
-# Printing script args
-print('sys.argv = ', sys.argv)
-
-# Checking OS version
+project_directory = (os.path.dirname(os.path.realpath(__file__)))
+logging_filename = project_directory + '/output.log'
+git_source = 'https://github.com/bigpandaio/ops-exercise'
+git_repo_name = git_source.split('/')[-1]
+s3_destination_directory = project_directory + '/' + git_repo_name + '/public/images'
+s3_download_url = 'https://s3.eu-central-1.amazonaws.com/devops-exercise/pandapics.tar.gz'
 
 
-# Checking for Docker
+# Configure logging
 
-
-# Installing Docker and Docker Compose
-
-
-# Checking open ports
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s  %(message)s",
+    handlers=[logging.FileHandler(logging_filename), logging.StreamHandler(stream=sys.stdout)]
+)
 
 
 # Cloning Git
-def git_clone():
+def func_git_clone(git_source, destination_dir):
     try:
-        git.Git(current_directory/ops-exercise).pull('git://github.com/bigpandaio/ops-exercise.git')
+        git.Git(destination_dir).clone(git_source)
     except Exception as e:
         logging.error(e)
 
-git_clone()
 
-# Downloading resources
-
-
-# Build Run and Compose UP
-
-
-# Tests
+# Git Pulling
+def func_git_pull(git_source, destination_dir):
+    try:
+        git.Git(destination_dir).pull(git_source)
+    except Exception as e:
+        logging.error(e)
 
 
-# The deployment flow script should perform the following steps:
-# Download image resources file from AWS S3 (https://s3.eu-central-1.amazonaws.com/devops-exercise/pandapics.tar.gz) and extract it's content to '/public/images' path.
-# Create, build & run the App + DB using “docker-compose up” command.
-# Check the App's health (See App's healthcheck below) at the end of the deployment flow and fail the deployment flow upon bad App health.
+# Download form S3
+def func_s3_download(s3_download_url, s3_destination_directory):
+    try:
+        stream_data = urllib.request.urlopen(s3_download_url)
+        tar_data = tarfile.open(fileobj=stream_data, mode="r|gz")
+        tar_data.extractall(s3_destination_directory)
+    except Exception as e:
+        logging.error(e)
 
 
+# Update Git local repo
+def func_git_update(git_source, destination_dir):
+    try:
+        if os.path.isdir(project_directory + '/' + git_repo_name):
+            func_git_pull(git_source, destination_dir)
+        else:
+            func_git_clone(git_source, destination_dir)
+    except Exception as e:
+        logging.error(e)
 
+
+if __name__ == "__main__":
+    func_git_update(git_source=git_source, destination_dir=project_directory)
+    func_s3_download(s3_download_url, s3_destination_directory)
+    print(s3_destination_directory)
 
