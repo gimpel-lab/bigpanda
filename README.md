26d792f8a9fcb3d7393df955c14ce35b71282ae3
Gimpel Lab <root@lab.gimpel.co.il>
2019-02-10 18:35:14 +0000

    done

--- README.md
+++ README.md
@@ -1,41 +1,74 @@
 # BigPanda
+## Task form the email:
+##### Your mission in this task is to implement a deployment flow of a containerized application and it's DB:
+- The deployment flow script needs to be coded using Python language (please specify which version you ran it with)
+- The App + DB containers need to be run using Docker and Docker-compose:
+- If you don't know docker-compose, don't worry - Go to https://docs.docker.com/compose/gettingstarted/#step-3-define-services-in-a-compose-file to view a guide on how to create a docker-compose.yml and run it.
+- You DO NOT need to create any Dockerfiles, they are already given to you in the App's repo (See “About the App” section below.
 
+##### The deployment flow script should perform the following steps:
+- Download image resources file from AWS S3 (https://s3.eu-central-1.amazonaws.com/devops-exercise/pandapics.tar.gz) and extract it's content to '/public/images' path.
+- Create, build & run the App + DB using “docker-compose up” command.
+- Check the App's health (See App's healthcheck below) at the end of the deployment flow and fail the deployment flow upon bad App health.
 
+##### Once done, please create a Github repo (Don't fork our repo) and send us a link to it. The repo should contain the following:
+- The deployment flow script.
+- The docker-compose.yml file.
+- Any additional files needed in order to run the app from it's root folder.
+- Proper documentation needed in order to fully run your exercise on our localhost.
+
+## Result:
+##### Tested on: 
 ```
-apt install -y git python3 python3-pip
+Ubuntu 18.04.2 LTS
+Python 3.6.7
+Git version 2.17.1
+Docker version 18.09.1, build 4c52b90
+docker-compose version 1.23.2, build 1110ad01
+```
 
+##### Install Python3 and PIP
+```
+sudo apt update
+sudo apt install -y git python3 python3-pip
+```
 
+##### Install Docker CE from docs for Ubuntu 18.04:
+https://docs.docker.com/install/linux/docker-ce/ubuntu/
+```
+sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+```
+
+##### Install Docker Compose from docs for Ubuntu 18.04:
+https://docs.docker.com/compose/install/
+```
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+```
+
+##### How to start:
+```
+git clone https://github.com/gimpel-lab/bigpanda
+cd bigpanda
+pip3 install -r requirements.txt
+python3 deployscript.py
 ```
 
 
-Structure:
+##### Structure of the project:
 ```
 rootdir/
-├── deploy.py
-├── req.txt
-├── git/ (will be git cloned during deploy)
-│   ├── bootstrap.js
-│   ├── 
-│   ├── 
-│   └── ...
-└── images/ (will be create during deploy)
-    ├── glyphicons-halflings-regular.eot
-    ├── glyphicons-halflings-regular.svg
-    ├── glyphicons-halflings-regular.ttf
-    └── glyphicons-halflings-regular.woff
-
-```
-
-Requirements:
-- Python 2.7+ 
-- Docker
-- Docker Compose
-- Git
-- Pip
-
-
-```
-apt update
-
+├── deployscript.py (The deployment flow script)
+├── requirements.txt (Additional file for depensies)
+├── docker-compose.yml (The docker-compose.yml file)
+└── public/ (The forlder will be downloaded and unpacked from gzip from s3)
+    └── images (will be mounted to ops-exercise container)
+        ├── bear-2316805_1920.png
+        ├── panda-1454345_1280.png
+        └── panda-2850869_1920.png
 ```
 
--- deployscript.py
+++ deployscript.py
@@ -1,71 +1,75 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import os
 import sys
 import logging
-import git
 import tarfile
 import urllib.request
-# import requests
-# import subprocess
-# import time
+import subprocess
+from time import sleep
 
 project_directory = (os.path.dirname(os.path.realpath(__file__)))
-logging_filename = project_directory + '/output.log'
-git_source = 'https://github.com/bigpandaio/ops-exercise'
-git_repo_name = git_source.split('/')[-1]
-s3_destination_directory = project_directory + '/' + git_repo_name + '/public/images'
+
+s3_destination_directory = project_directory + '/public/images'
 s3_download_url = 'https://s3.eu-central-1.amazonaws.com/devops-exercise/pandapics.tar.gz'
 
+health_check_url = 'http://localhost:3000/health'
+health_check_retries = 10
+health_check_timeout = 6
 
-# Configure logging
+logging_filename = project_directory + '/output.log'
+logging_level = logging.INFO
+logging_format = '%(asctime)s %(name)s: %(message)s'
+
 
 logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s  %(message)s",
+    level=logging_level,
+    format=logging_format,
     handlers=[logging.FileHandler(logging_filename), logging.StreamHandler(stream=sys.stdout)]
 )
 
 
-# Cloning Git
-def func_git_clone(git_source, destination_dir):
-    try:
-        git.Git(destination_dir).clone(git_source)
-    except Exception as e:
-        logging.error(e)
-
-
-# Git Pulling
-def func_git_pull(git_source, destination_dir):
-    try:
-        git.Git(destination_dir).pull(git_source)
-    except Exception as e:
-        logging.error(e)
-
-
-# Download form S3
 def func_s3_download(s3_download_url, s3_destination_directory):
+    logging.info('Downloading:' + s3_download_url + 'to:' + s3_destination_directory)
     try:
-        stream_data = urllib.request.urlopen(s3_download_url)
-        tar_data = tarfile.open(fileobj=stream_data, mode="r|gz")
-        tar_data.extractall(s3_destination_directory)
+        tarfile.open(fileobj=urllib.request.urlopen(s3_download_url), mode="r|gz").extractall(s3_destination_directory)
     except Exception as e:
         logging.error(e)
+        sys.exit(e)
 
 
-# Update Git local repo
-def func_git_update(git_source, destination_dir):
+def func_exec_shell(*args):
+    logging.info('Executing:' + str(args))
     try:
-        if os.path.isdir(project_directory + '/' + git_repo_name):
-            func_git_pull(git_source, destination_dir)
-        else:
-            func_git_clone(git_source, destination_dir)
+        subprocess.call([*args])
     except Exception as e:
         logging.error(e)
+        sys.exit(e)
+
+
+def func_health_check(health_check_url):
+    logging.info('Starting health check')
+    for retry in range(health_check_retries):
+        try:
+            health_check_status = urllib.request.urlopen(health_check_url).getcode()
+        except Exception as e:
+            health_check_status = 0
+        if health_check_status == 200:
+            logging.info('Health check is ok')
+            return True
+        logging.error('Health check fail, retries:' + str(retry + 1) + '/' + str(health_check_retries))
+        sleep(health_check_timeout)
+    return False
+
+
+def main():
+    func_s3_download(s3_download_url, s3_destination_directory)
+    func_exec_shell('docker-compose', 'rm', '--stop', '--force')
+    func_exec_shell('docker-compose', 'up', '-d', '--force-recreate')
+    if func_health_check(health_check_url):
+        logging.info('Success deploying the app')
 
 
 if __name__ == "__main__":
-    func_git_update(git_source=git_source, destination_dir=project_directory)
-    func_s3_download(s3_download_url, s3_destination_directory)
-    print(s3_destination_directory)
-
+    logging.info('Starting deploy flow')
+    main()
