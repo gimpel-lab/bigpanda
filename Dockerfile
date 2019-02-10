e643bde89dacff35ccd6df2283d2de382145a787
root <root@bigpanda.gimpel.co.il>
2019-02-10 11:25:12 +0000

    the first check

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:alpine
-
-ADD . /opt/app
-WORKDIR /opt/app
-
-RUN npm install
-
-CMD ["npm", "start"] 
---
+++ deployscript.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+# Printing script args
+print('sys.argv = ', sys.argv)
+
+# Checking OS version
+
+
+# Checking for Docker
+
+
+# Installing Docker and Docker Compose
+
+
+# Checking open ports
+
+
+# Cloning Git
+
+
+# Downloading resources
+
+
+# Build Run and Compose UP
+
+
+# Tests
+
+
+# The deployment flow script should perform the following steps:
+# Download image resources file from AWS S3 (https://s3.eu-central-1.amazonaws.com/devops-exercise/pandapics.tar.gz) and extract it's content to '/public/images' path.
+# Create, build & run the App + DB using “docker-compose up” command.
+# Check the App's health (See App's healthcheck below) at the end of the deployment flow and fail the deployment flow upon bad App health.
+
+
+
+
